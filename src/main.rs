397e155dbb26e9fdd808d02e74f8bbf0186f0f29
Yoichi Ojima <yoichiojima@gmail.com>
2024-05-02 23:35:36 +0900

    made app interactive

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,31 @@ mod api_client;
 mod message;
 
 use message::{Message, Role};
+use std::io;
 
 #[tokio::main]
 async fn main() {
-    let mut messages = vec![Message {
-        role: Role::User,
-        content: "this is a test".to_string(),
-    }];
-    let first_choice = api_client::get_first_choice(&messages).await.unwrap();
-    messages.push(Message {
-        role: Role::Assistant,
-        content: first_choice,
-    });
-    println!("{:?}", messages);
+    let mut messages: Vec<Message> = Vec::new();
+
+    loop {
+        let mut input = String::new();
+
+        io::stdin().read_line(&mut input).unwrap();
+
+        messages.push(Message {
+            role: Role::User,
+            content: input,
+        });
+
+        let first_choice = api_client::get_first_choice(&messages).await.unwrap();
+
+        println!("{}", first_choice);
+
+        messages.push(Message {
+            role: Role::Assistant,
+            content: first_choice,
+        });
+
+        println!("");
+    }
 }
