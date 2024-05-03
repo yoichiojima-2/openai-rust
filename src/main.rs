8367f55b11e7bc76d32ff2aafeeff8772bedff86
Yoichi Ojima <yoichiojima@gmail.com>
2024-05-03 10:19:35 +0900

    add translator

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
 mod api_client;
 mod message;
 
-use message::{Message, Role};
 use std::io;
+use clap::{Command, Arg};
+use message::{Message, Role};
 
 #[tokio::main]
 async fn main() {
-    interactive_chat().await;
+    let matches = Command::new("openai-rust")
+        .version("2024.5.3")
+        .author("Yoichi Ojima <yoichiojima@gmail.com>")
+        .about("OpenAI API client written in Rust")
+        .subcommand(Command::new("interactive")
+            .about("Start interactive chat")
+        )
+        .subcommand(Command::new("translate")
+            .about("Translate given text to Japanese")
+            .arg(Arg::new("text")
+                .help("Text to translate")
+                .required(true)
+                .value_parser(clap::value_parser!(String))
+            )
+        )
+        .get_matches();
+    
+    match matches.subcommand() {
+        Some(("interactive", _)) => interactive_chat().await,
+        Some(("translate", args)) => translate(args.get_one::<String>("text").unwrap()).await,
+        _ => println!("Please specify a subcommand"),
+    
+    }
 }
 
 async fn interactive_chat() {
@@ -35,4 +58,22 @@ async fn interactive_chat() {
 
         println!("");
     }
+}
+
+async fn translate(text: &str) {
+    let messages: Vec<Message> = vec![
+        Message {
+            role: Role::System,
+            content: "Translate given text to Japanese".to_string(),
+        },
+        Message {
+            role: Role::User,
+            content: text.to_string(),
+        }
+    ];
+
+    let response = api_client::request(&messages).await.unwrap();
+    let first_choice = api_client::get_first_choice(&response).await.unwrap();
+
+    println!("{}", first_choice);
 }
