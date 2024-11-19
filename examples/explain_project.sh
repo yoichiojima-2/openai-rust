6fc49684b8e21a9f1495c6306fb9ba082ab3ad80
Yoichi Ojima <yoichiojima@gmail.com>
2024-11-19 22:10:28 +0900

    update examples

--- examples/explain_project.sh
+++ examples/explain_project.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
+# simply pack all the information and pass openai-rust ask for general questions
 introductory_message="i will give you a output of tree . && find . -name \"*.py\" -exec cat {} \; explain what the project is doing"
-
 (echo ${introductory_message} && tree . && find . -name "*.py" -exec cat {} \;) | xargs -0 openai-rust ask
