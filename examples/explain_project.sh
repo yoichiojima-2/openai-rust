7146dd0a2756af39069fc1e2e79013e30342e477
Yoichi Ojima <yoichiojima@gmail.com>
2024-11-19 22:06:51 +0900

    update examples

--- examples/explain_project.sh
+++ examples/explain_project.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-introductory_message="i will give you a return  tree . && find . -name \"*.py\" -exec cat {} \; explain what this project is doing"
+introductory_message="i will give you a return  tree . && find . -name \"*.py\" -exec cat {} \; explain what the project is doing"
 
 (echo ${introductory_message} && tree . && find . -name "*.py" -exec cat {} \;) | xargs -0 openai-rust ask
